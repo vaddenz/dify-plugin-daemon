4a761aa5b220455fbfefe45cd58d17101eb9941a
Yeuoly <admin@srmxy.cn>
2024-09-27 21:04:09 +0800

    feat: implement prompt message tool call

--- internal/types/entities/model_entities/llm.go
+++ internal/types/entities/model_entities/llm.go
@@ -97,7 +97,12 @@ type PromptMessageContent struct {
 }
 
 type PromptMessageToolCall struct {
-	// TODO:
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Function struct {
+		Name      string `json:"name"`
+		Arguments string `json:"arguments"`
+	} `json:"function"`
 }
 
 func init() {
