63f1688fa4184bc7cfccf53aada33383c24d1405
Yeuoly <admin@srmxy.cn>
2025-01-07 18:44:35 +0800

    fix: add credentials to ValidateCredentialsResult

--- pkg/entities/model_entities/validate.go
+++ pkg/entities/model_entities/validate.go
@@ -1,5 +1,6 @@
 package model_entities
 
 type ValidateCredentialsResult struct {
-	Result bool `json:"result"`
+	Result      bool           `json:"result"`
+	Credentials map[string]any `json:"credentials"`
 }
