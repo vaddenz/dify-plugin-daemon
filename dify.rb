47f0b7e65573597b27d8173aa36439cd4d91f8af
crazywoola <427733928@qq.com>
2025-01-09 23:29:10 +0800

    feat: bump version

--- dify.rb
+++ dify.rb
@@ -1,22 +1,22 @@
 class Dify < Formula
   desc "Dify"
   homepage "https://github.com/langgenius/dify-plugin-daemon"
-  version "0.0.1-beta.21"
+  version "0.0.1-beta.23"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.21/dify-plugin-darwin-amd64"
+      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.23/dify-plugin-darwin-amd64"
     elsif Hardware::CPU.arm?
-      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.21/dify-plugin-darwin-arm64"
+      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.23/dify-plugin-darwin-arm64"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.21/dify-plugin-linux-amd64"
+      url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.23/dify-plugin-linux-amd64"
     elsif Hardware::CPU.arm?
-      url "https://github.com/langgenius/dify-plugin-ddify aemon/releases/download/0.0.1-beta.21/dify-plugin-linux-arm64"
+      url "https://github.com/langgenius/dify-plugin-ddify aemon/releases/download/0.0.1-beta.23/dify-plugin-linux-arm64"
     end
   elsif OS.windows?
-    url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.21/dify-plugin-windows-amd64"
+    url "https://github.com/langgenius/dify-plugin-daemon/releases/download/0.0.1-beta.23/dify-plugin-windows-amd64"
   end
 
   def install
