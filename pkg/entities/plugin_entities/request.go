4dbea42603dae01808636263bd23610b7b4a08b5
Yeuoly <45712896+Yeuoly@users.noreply.github.com>
2025-02-28 12:48:00 +0800

    refactor: make user ID optional in plugin user identity (#41)

--- pkg/entities/plugin_entities/request.go
+++ pkg/entities/plugin_entities/request.go
@@ -2,7 +2,7 @@ package plugin_entities
 
 type InvokePluginUserIdentity struct {
 	TenantId string `json:"tenant_id" validate:"required" uri:"tenant_id"`
-	UserId   string `json:"user_id" validate:"required"`
+	UserId   string `json:"user_id"`
 }
 
 type BasePluginIdentifier struct {
